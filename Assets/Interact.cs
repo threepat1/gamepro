057785dbe581d46225f535f29c2e54039b733ac2
jtojung <42223493+jtojung@users.noreply.github.com>
2018-08-22 10:02:09 +1000

    add gameprogram

---
+++ Assets/Interact.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+
+    [Header("References")]
+    public GameObject player;
+    public GameObject mainCam;
+
+    void Start()
+    {
+
+
+        player = GameObject.Find("Player");
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+
+
+    /// cam = mainCam.GetComponent<Camera>();
+    /// cam = GameObject.FindGameObjectWithTag("MainCamer").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray Interact;
+            Interact = Camera.main.ScreenPointToRay
+                (new Vector2(Screen.width / 2, Screen.height / 2));
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Interact, out hitInfo, 10.0f))
+            {
+                #region NPC Dialogue
+                if (hitInfo.collider.CompareTag("NPC"))
+                {
+                    Debug.Log("Talk to NPC");
+                }
+
+                #endregion
+                #region Chest
+                if (hitInfo.collider.CompareTag("Chest"))
+                {
+                    Debug.Log("Open Chest");
+                }
+
+                #endregion
+                #region Item
+                if (hitInfo.collider.CompareTag("Item"))
+                {
+                    Debug.Log("Pick up Item");
+                }
+                #endregion
+            }
+
+        }
+
+
+    }
+}
